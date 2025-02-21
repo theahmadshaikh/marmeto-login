9847e13868d581983cdbcb0f08e526221b41e890
Ahmad Shaikh <ahmadshaikh2026@gmail.com>
2025-02-21 17:15:30 +0530

    done

---
+++ scripts/script.js
@@ -0,0 +1,74 @@
+const loginContainer = document.querySelector(".login-container")
+const otpContainer = document.querySelector(".otp-container")
+const mainPage = document.querySelector(".main-page")
+const loginForm = document.querySelector("#login-form")
+const email = document.querySelector("#email")
+const phoneNumber = document.querySelector("#phone-number")
+const emailError = document.querySelector("#email-error")
+const phoneNumberError = document.querySelector("#phone-number-error")
+const confirmButton = document.querySelector("#confirm-btn")
+const backBtn = document.querySelector(".back-btn");
+const userNameElement = document.querySelector("#user-name")
+let userName ="User";
+
+
+let allowedDomain = "marmeto.com";
+let emailPattern = new RegExp(`^[a-zA-Z0-9._%+-]+@${allowedDomain}$`);
+let phonePattern = /^[6-9]\d{9}$/;
+
+const handelSubmit = (e)=>{
+    e.preventDefault();
+    emailError.classList.add("hidden")
+    phoneNumberError.classList.add("hidden")
+    if(!emailPattern.test(email.value))
+       return emailError.classList.toggle("hidden")
+    if(!phonePattern.test(phoneNumber.value))
+        return phoneNumberError.classList.toggle("hidden") 
+    userName = email.value.split("@")[0];
+    loginContainer.classList.toggle("hidden")
+    otpContainer.classList.toggle("hidden")
+}
+
+
+const handelSubmitOtp = (e)=>{
+    const digit1 = document.querySelector("#digit1")
+    const digit2 = document.querySelector("#digit2")
+    const digit3 = document.querySelector("#digit3")
+    const digit4 = document.querySelector("#digit4")
+    const otp = digit1.value+digit2.value+digit3.value+digit4.value;
+    if(otp!=="2025")
+       return alert("Please enter valid otp");
+    otpContainer.classList.toggle("hidden")
+    mainPage.classList.toggle("hidden")
+    userNameElement.textContent = `Hey ${userName}!`
+    
+}
+
+
+window.addEventListener("DOMContentLoaded",()=>{
+    loginForm.addEventListener("submit",handelSubmit)
+    confirmButton.addEventListener("click",handelSubmitOtp)
+    
+        let inputs = document.querySelectorAll(".otp-btns input");
+    
+        inputs.forEach((input, index) => {
+            input.addEventListener("input", function () {
+                if(this.value.length === 1 && index===inputs.length-1)
+                    confirmButton.focus()
+                if (this.value.length === 1 && index < inputs.length - 1) {
+                    inputs[index + 1].focus(); 
+                }
+            });
+    
+            input.addEventListener("keydown", function (event) {
+                if (event.key === "Backspace" && index > 0 && this.value === "") {
+                    inputs[index - 1].focus(); 
+                }
+            });
+        });
+        backBtn.addEventListener("click",()=>{
+            loginContainer.classList.toggle("hidden")
+    otpContainer.classList.toggle("hidden")
+        })
+    
+})
